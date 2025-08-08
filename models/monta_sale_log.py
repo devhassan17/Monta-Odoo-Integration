8daa9e1d72f65cbe35ee91dfbc8306f3a76825c8
devhassan17 <156808621+devhassan17@users.noreply.github.com>
2025-08-09 00:39:59 +0500

    Update monta_sale_log.py

--- models/monta_sale_log.py
+++ models/monta_sale_log.py
@@ -6,16 +6,16 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    @api.model
-    def create(self, vals):
-        order = super().create(vals)
+    def action_confirm(self):
+        res = super().action_confirm()
 
-        partner = order.partner_id
-        _logger.info("🛒 New Sales Order Created:")
-        _logger.info(f"📄 Order: {order.name}")
-        _logger.info(f"👤 Customer: {partner.name}")
-        _logger.info(f"✉️ Email: {partner.email}")
-        _logger.info(f"💰 Total: {order.amount_total}")
-        _logger.info(f"🛍️ Order Lines: {[(l.product_id.name, l.product_uom_qty) for l in order.order_line]}")
+        for order in self:
+            partner = order.partner_id
+            _logger.info("✅ Order Confirmed:")
+            _logger.info(f"📄 Order: {order.name}")
+            _logger.info(f"👤 Customer: {partner.name}")
+            _logger.info(f"✉️ Email: {partner.email}")
+            _logger.info(f"💰 Total: {order.amount_total}")
+            _logger.info(f"🛍️ Order Lines: {[(l.product_id.name, l.product_uom_qty) for l in order.order_line]}")
 
-        return order
+        return res
