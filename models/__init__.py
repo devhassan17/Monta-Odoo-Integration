c2438a63684760d953d7478c40e52935584c865b
devhassan17 <156808621+devhassan17@users.noreply.github.com>
2025-08-28 18:35:49 +0500

    Update

--- models/__init__.py
+++ models/__init__.py
@@ -5,3 +5,8 @@ from . import product_template
 from . import sale_order
 from . import sale_order_line
 from . import sku_test_log
+from . import sale_order_inbound
+from . import inbound_expected_delivery       
+from . import product_stock_policies          
+from . import order_batch_trace               
+from . import sale_order_status_map 
