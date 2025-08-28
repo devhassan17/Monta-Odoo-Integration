b1d7a862d21773fc0715931b462643c9da98fb1a
devhassan17 <156808621+devhassan17@users.noreply.github.com>
2025-08-28 13:28:55 +0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Monta-Odoo Integration',
     'version': '1.0.0',
-    'author': 'Ali Hassan Muddassir',
+    'author': 'Ali Hassan Malik',
     'category': 'Sales',
     'summary': 'Step 1: Log order creation for Monta integration',
     'depends': ['sale_management', "mrp"],
