25fe40edc9e04cf8e8d51ff6795a85560d86c8d4
devhassan17 <156808621+devhassan17@users.noreply.github.com>
2025-09-07 00:53:02 +0500

    Update

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Monta-Odoo Integration",
     "version": "1.0.0",
-    "author": "Ali Hassan19",
+    "author": "Ali Hassan20",
     "category": "Sales",
     "summary": "Monta WMS integration: Orders, EDD, Inbound Forecast",
     "depends": ["sale_management", "mrp", "purchase"],
