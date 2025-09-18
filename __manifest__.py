a459f0348d5b20a5474876e2dad51945a2f8110a
devhassan17 <156808621+devhassan17@users.noreply.github.com>
2025-09-18 06:12:05 +0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     "application": True,
     "license": "LGPL-3",
     "post_init_hook": "post_init_hook",
-    "uninstall_hook": "uninstall_hook",
+    # "uninstall_hook": "uninstall_hook",
     "assets": {
         "web.assets_backend": [
             "Monta-Odoo-Integration/static/src/js/commitment_autofill.js",
